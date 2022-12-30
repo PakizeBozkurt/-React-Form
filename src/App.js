1d95ef7c7b254cbfc2dffdff91674a2b0a329e39
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-30 18:04:11 +0000

    React Forms

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import "./index.css";
+
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    console.log("Changed");
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    console.log("Clicked");
+    setHeading(name);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <form onSubmit={handleClick}>
+        <h1>Hello {headingText} </h1>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          // value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
